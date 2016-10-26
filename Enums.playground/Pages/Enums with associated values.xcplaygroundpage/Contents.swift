1c0387c1cd13d3f97f824f76ba7c11954a6e363a
Nikolas Burk <nikolas.burk@gmail.com>
2016-10-26 10:23:09 -0700

    initial commit

---
+++ Enums.playground/Pages/Enums with associated values.xcplaygroundpage/Contents.swift
@@ -0,0 +1,45 @@
+//: Playground - noun: a place where people can play
+
+class Person {
+  let name: String
+  
+  init(name: String) {
+    self.name = name
+  }
+}
+
+enum MassageChair {
+  case free
+  case occupied(Person)
+}
+
+let nikolas = Person(name: "Nikolas")
+let massageChair = MassageChair.occupied(nikolas)
+
+switch massageChair {
+case .free:
+  print("The chair is currently free.")
+case .occupied(let person):
+  print("\(person.name) is enjoying an extremyle nice and comforting massage right now. Please don't disturb and wait until it's your turn!")
+}
+
+/*:
+ 
+ ### Challenges
+ 
+ 1. Model a type called `Box` that is either empty or contains exactly one `Thing`. `Thing` looks as follows:
+ ```
+ class Thing {
+   let name: String
+ 
+   init(name: String) {
+     self.name = name
+   }
+ }
+ ```
+ 2. Create an empty `Box`
+ 3. Create three different `Box`es, each should contain one item of the following: `Soccer Ball`, `Phone`, `Chocolate`
+ 4. Write a function that takes a `Box` as an argument and returns a `String` indicating what that `Box` contains.
+ 5. Make the `Box` _generic_ so that it can contain an object of any type, not just `Thing`.
+ */
+
